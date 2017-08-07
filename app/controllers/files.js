7496fc7a089a32be65ec6903e40e19045b95bfc5
lauramv21 <laura_mv21@hotmail.com>
2017-08-07 16:26:38 -0500

    no /

--- app/controllers/files.js
+++ app/controllers/files.js
@@ -24,7 +24,7 @@ module.exports = function (app) {
  URI: /publicar
  */
 
-router.get('/publicar', ensureAuthenticated, function(req, res){
+router.get('publicar', ensureAuthenticated, function(req, res){
   res.render('upload', {baseUrl: config.baseUrl});
 });
 
@@ -32,7 +32,7 @@ router.get('/publicar', ensureAuthenticated, function(req, res){
  Método: POST
  URI: /publicando
  */
-router.post("/publicando", upload.single('video'), function(req,res){
+router.post("publicando", upload.single('video'), function(req,res){
   var private = false;
   if(req.body.privateFile == 'on'){
     private = true;
@@ -66,7 +66,7 @@ router.post("/publicando", upload.single('video'), function(req,res){
  URI: /videos
  */
 
-router.get("/videos",ensureAuthenticated,function(req, res){
+router.get("videos",ensureAuthenticated,function(req, res){
   File.find({privateFile:"false"},function(err, documento){
     if(err){console.log(err);}
     res.render("display",{ videos : documento}, {baseUrl: config.baseUrl})
@@ -78,7 +78,7 @@ router.get("/videos",ensureAuthenticated,function(req, res){
  URI: /misvideos
  */
 
-router.get('/misvideos', ensureAuthenticated, function(req, res){
+router.get('misvideos', ensureAuthenticated, function(req, res){
   File.find({username:req.user.username}, function(err, documento){
     if(err){console.log(err);}
     console.log(documento);
@@ -91,14 +91,14 @@ router.get('/misvideos', ensureAuthenticated, function(req, res){
  URI: /editar/:id
  */
 
-router.get('/editar/:id', ensureAuthenticated, function(req, res) {
+router.get('editar/:id', ensureAuthenticated, function(req, res) {
   var id_video = req.params.id;
   File.findOne({"_id": id_video}, function (err, video) {
     res.render('edit', {video:video}, {baseUrl: config.baseUrl});
   });
 });
 
-router.post('/editar/:id', function(req, res){
+router.post('editar/:id', function(req, res){
   var privado = false;
   if(req.body.privateFile == 'on'){
     privado = true;
@@ -110,7 +110,7 @@ router.post('/editar/:id', function(req, res){
   };
   console.log(videoData);
   File.update({"_id":req.params.id}, videoData, function(){
-    res.redirect("/misvideos", {baseUrl: config.baseUrl})
+    res.redirect("misvideos", {baseUrl: config.baseUrl})
   });
 });
 
@@ -119,7 +119,7 @@ router.post('/editar/:id', function(req, res){
  URI: /buscar
  */
 
-router.post('/buscar', function(req, res) {
+router.post('buscar', function(req, res) {
   File.find({username: req.body.buscar, privateFile: "false"}, function (err, documento) {
     if (err) {
       console.log(err);
@@ -133,6 +133,6 @@ function ensureAuthenticated(req, res, next){
     return next();
   } else {
     //req.flash('error_msg','You are not logged in');
-    res.redirect('/ingresar', {baseUrl: config.baseUrl});
+    res.redirect('ingresar', {baseUrl: config.baseUrl});
   }
 }
