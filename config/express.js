54692faac5ae787d09f3718997f23cc23481461e
lauramv21 <laura_mv21@hotmail.com>
2017-08-07 18:05:58 -0500

    express

--- config/express.js
+++ config/express.js
@@ -23,7 +23,7 @@ module.exports = function(app, config) {
   }));
 
   Handlebars.registerHelper('ifCond', function(v1, v2, options) {
-    if(v1 === v2) {
+    if(v1 == v2) {
       return options.fn(this);
     }
     return options.inverse(this);
